75d604956d7d438d6e4057ac642a392061de116c
Nischal Shakya <nischalshakya2015@gmail.com>
2022-10-18 20:27:00 +0545

    refactor: contract related changes
    - Add contract abi
    - Add constants related to contract and wallet

---
+++ src/data/constants.ts
@@ -0,0 +1,25 @@
+export const ALCHEMY_KEY =
+  "wss://polygon-mumbai.g.alchemy.com/v2/hHVhPNYKEaOZjvwDwT1zzB3QU6OXSr3X";
+
+export const CONTRACT_ADDRESS = "0x924040f18aeaeb71ce4549032778fc9825a569c8";
+
+export const OPENSEA_ASSETS_URL = "https://testnets.opensea.io/assets/mumbai";
+
+export const CLIENT_ID =
+  "BLUuPNeRyjHfyDDPdfhmUg1KeeQJuwFI8GF4lFO6qCQOZfhcchliU8MZM1kCfc9DpRXL7M0QUWo23kN2OF-My0Q";
+
+export const RPC_TARGET =
+  "https://polygon-mumbai.g.alchemy.com/v2/hHVhPNYKEaOZjvwDwT1zzB3QU6OXSr3X";
+
+export const APP_LOGO = "https://i.ibb.co/2gmPw9f/twitter-Dapp-Logo.jpg";
+
+export const ADAPTER_TWITTER_CLIENT_VERIFIER = "twitter-dapp-nischal";
+
+export const ADAPTER_TWITTER_CLIENT_ID = "u9uW4T193pNKKJ3JonHkhN3HfwuTU2lA";
+
+export const AUTH0_DOMAIN = "dev-hayl5soi.us.auth0.com";
+
+export const BIRD_IMAGE =
+  "https://i.ibb.co/42WNxRN/reshot-icon-bird-TL39-D5-P8-YH.png";
+
+export const REACT_APP_REFRESH_TIMER = 30;
